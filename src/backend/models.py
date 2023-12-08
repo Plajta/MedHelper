38843e4bdb19d44ecd489328740fd2d8d04abf7e
Vesek <venda.straka@gmail.com>
2023-12-08 17:43:07 +0100

    Add Database models

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,9 +1,21 @@
 from flask_login import UserMixin
 from . import db
 
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(255))
+
+class Placement(db.Model):
+    id = db.Column(db.Integer, unique=True)
+    placement = db.Column(db.String(255))
+    patient_id = db.Column(db.Integer, unique=True)
+
+class Messages(db.Model):
+    sender_id = db.Column(db.Integer, unique=True)
+    body = db.Column(db.Text)
