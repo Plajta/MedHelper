6c4c29da48ced2211f6e075f43bc4a5587283c7b
HelloWorld7894 <danpojhan@protonmail.com>
2023-12-08 16:24:58 +0100

    SOCKETIO ROUTING BABYYY

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, redirect, request
+from flask_socketio import SocketIO, emit
 
 app = Flask("PlajtaMed", template_folder='src/frontend/templates', static_url_path='/static', static_folder='src/frontend/static')
+socketio = SocketIO(app)
+
+#
+# APP ROUTES
+#
 
 @app.route('/')
 def index():
@@ -13,8 +19,8 @@ def admin():
         lastname = request.form['lname']
         birth = request.form['birth']
 
-        print(name)
 
+        #update admin windows
 
     return render_template('admin.html', user="Damongus Veliký", position="Bůh všehomíra", auth="69")
 
@@ -32,3 +38,24 @@ def login():
         return "Blbečku!"
     else:
         return render_template('login.html')
+    
+
+
+#
+# SOCKETIO ROUTES
+#
+
+@socketio.on("admin-event")
+def handle_message_admin(data):
+    if data == "send-patients":
+        print("Send patients!")
+
+        patient_array = [{
+            "name": "františek hřímal",
+            "desc": "Haha popis"
+        }]
+
+        emit("patients-data", {"data": patient_array})
+
+if __name__ == "__main__":
+    socketio.run(app, allow_unsafe_werkzeug=True)
--- src/frontend/static/admin.js
+++ src/frontend/static/admin.js
@@ -1,6 +1,46 @@
 window.onload = () => {
+    var socket = io();
+
+    //socket.io event listeners
+    socket.on("connect", () => {
+        //fetch all patients
+        socket.emit("admin-event", "send-patients")
+
+    })
+
+    socket.on("patients-data", (data) => {
+        console.log(data)
+        let patients_div = document.getElementById("patient-list")
+
+        for (let i = 0; i < data["data"].length; i++){
+            console.log(data["data"][i])
+            //create patient record
+            let patient = document.createElement("div")
+            patient.classList.add("patient")
+
+            let header = document.createElement("h3")
+            header.innerHTML = `Jméno: <span>${data["data"][i]["name"]}</span>`
+            
+            let description = document.createElement("p")
+            description.innerHTML = data["data"][i]["desc"]
+
+            let button = document.createElement("button")
+            button.innerHTML = "Odstranit"
+            button.classList.add("but")
+
+            patient.appendChild(header)
+            patient.appendChild(description)
+            patient.appendChild(button)
+
+            patients_div.appendChild(patient)
+        }
+    })
+
     //event listeners
-    document.querySelector("confirm-but").addEventListener("click", (event) => {
+    document.querySelector("#confirm-but").addEventListener("click", (event) => {
         document.getElementById("confirm-form").submit()
+
+        //fetch all patients
+        socket.emit("admin-event", "send-patients")
     })
 }
