00e37b7cd76843acb9c534c54dfabcf9d48f7a05
LosVocelos <janocelik@gmail.com>
2023-12-08 16:12:17 +0100

    Lol, u gotta rebase, haha!

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 
 app = Flask("PlajtaMed", template_folder='src/frontend/templates', static_url_path='/static', static_folder='src/frontend/static')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 @app.route('/')
 def index():
     return render_template('main.html')
+
 
 @app.route('/admin', methods=['POST', 'GET'])
 def admin():
@@ -15,20 +21,20 @@ def admin():
 
         print(name)
 
-
     return render_template('admin.html', user="Damongus Veliký", position="Bůh všehomíra", auth="69")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        user = request.form['username']
-        pwd = request.form['password']
-        if user == "amogus" and pwd == "ballz":
-            return redirect("admin")
-        return "Blbečku!"
-    else:
-        return render_template('login.html')
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('admin'))
+    return render_template('login.html', error=error)
