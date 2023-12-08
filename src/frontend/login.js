b3fbe844af4381aea924f49177f5ea4f2b46cfc5
HelloWorld7894 <danpojhan@protonmail.com>
2023-12-08 14:27:41 +0100

    xd sumci v hrnci

---
+++ src/frontend/login.js
@@ -0,0 +1,11 @@
+//functions
+function send_data(){
+
+}
+
+window.onload = () => {
+    //event listeners
+    document.querySelector("button.login-but").addEventListener("click", () => {
+        send_data()
+    })
+}
