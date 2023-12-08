0409733aeec824fca280c5146966bfed8d60d744
HelloWorld7894 <danpojhan@protonmail.com>
2023-12-08 14:43:41 +0100

    added forms

--- src/frontend/static/login.js
+++ src/frontend/static/login.js
@@ -1,11 +1,6 @@
-//functions
-function send_data(){
-
-}
-
 window.onload = () => {
     //event listeners
-    document.querySelector("button.login-but").addEventListener("click", () => {
-        send_data()
+    document.querySelector("button.login-but").addEventListener("click", (event) => {
+        document.getElementById("login-form").submit()
     })
 }
