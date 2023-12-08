6d29047fa91c9b6ddaa88050cfdd97e8896a894d
Vesek <venda.straka@gmail.com>
2023-12-08 18:00:43 +0100

    Fix models

--- src/backend/models.py
+++ src/backend/models.py
@@ -8,14 +8,14 @@ class Doctor(db.Model):
     name = db.Column(db.String(1000))
 
 class Patient(db.Model):
-    id = db.Column(db.Integer, unique=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(255))
 
 class Placement(db.Model):
-    id = db.Column(db.Integer, unique=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     placement = db.Column(db.String(255))
     patient_id = db.Column(db.Integer, unique=True)
 
 class Messages(db.Model):
-    sender_id = db.Column(db.Integer, unique=True)
+    sender_id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
