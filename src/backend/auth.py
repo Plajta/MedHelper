d48fc9c8c76ee7aaafd7ced9e1c0f9e3dd94a98f
Vesek <venda.straka@gmail.com>
2023-12-08 19:55:09 +0100

    Add rank and level to signup

--- src/backend/auth.py
+++ src/backend/auth.py
@@ -19,7 +19,7 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
-    user = Doctor.query.filter_by(email=username).first()
+    user = Doctor.query.filter_by(username=username).first()
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
@@ -40,18 +40,20 @@ def signup():
 @auth.route('/signup', methods=['POST'])
 def signup_post():
 
-    email = request.form.get('email')
-    name = request.form.get('name')
+    username = request.form.get('username')
+    displayname = request.form.get('displayname')
     password = request.form.get('password')
+    rank = request.form.get('rank')
+    level = request.form.get('level')
 
-    user = Doctor.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = Doctor.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again  
-        flash('Email address already exists')
+        flash('Username address already exists')
         return redirect(url_for('auth.signup'))
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = Doctor(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = Doctor(username=username, displayname=displayname, password=generate_password_hash(password, method='sha256'), rank=rank, level=level)
 
     # add the new user to the database
     db.session.add(new_user)
